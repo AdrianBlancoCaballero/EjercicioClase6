6e9723322a17b40098a66c17a95be11879ef49a8
Adrián Blanco <adrianblancocaballero@gmail.com>
2021-10-27 11:54:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Ejercicio de Clase 6
+
+word = input("Introduzca la/s palabra/s: ")
+vowels = ["a", "e", "i", "o", "u"]
+v_freq = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
+for letter in word:
+    if letter in vowels:
+        v_freq[letter] += 1
+print("Número de vocales en la palabra", word, ":", v_freq)
