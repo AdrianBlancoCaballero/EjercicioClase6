fbe3f0de9345e6635a249a8444a9ef886a007b3d
Adrián Blanco <adrianblancocaballero@gmail.com>
2021-10-27 11:56:04 +0200

    ec6

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 word = input("Introduzca la/s palabra/s: ")
 vowels = ["a", "e", "i", "o", "u"]
 v_freq = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
+
 for letter in word:
     if letter in vowels:
         v_freq[letter] += 1
+
 print("Número de vocales en la palabra", word, ":", v_freq)
